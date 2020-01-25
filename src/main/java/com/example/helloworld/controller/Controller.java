8584cd903ebc0276bc26fe853412114172621af4
FilsAime, Jonathan (plp2675) <jonathan.filsaime@usaa.com>
2020-01-24 18:47:19 -0600

    Initial commit

---
+++ src/main/java/com/example/helloworld/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Controller {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public Map<String, String> hello(){
+
+        Map value = new HashMap();
+        value.put("value", "hello world!");
+
+        return value;
+    }
+}
